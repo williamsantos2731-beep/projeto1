c1e9ddb5e4fbd39b0573cee8dd1f0eb80d1d586b
williamsantos2731-beep <williamsantos2731@gmail.com>
2025-10-25 13:40:54 -0300

    Add files via upload

---
+++ app01.java
@@ -0,0 +1,9 @@
+import L02Ex5.ContaBancaria;
+
+public class app01 {
+    public static void main(String[] args) {
+        ContaBancaria conta1 = new ContaBancaria(10000);
+        double novoSalario = conta1.depositar(500);
+        
+    }
+}
